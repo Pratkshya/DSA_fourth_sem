ca068368bba826d1f7871dd1d751cb1129a3ffea
Pratkshya <pratikshyaparajuli37@gmail.com>
2024-12-31 11:15:42 +0545

    lab3

---
+++ factorialLab3.c
@@ -0,0 +1,32 @@
+//Factorial using recursion
+#include <stdio.h>
+int fact(int n)
+{
+  if (n == 1)
+  {
+    return 1;
+  }
+  else
+  {
+    return n * fact(n-1);
+  }
+}
+int main()
+{
+  int n;
+  printf("Enter any number: ");
+  scanf("%d", &n);
+  if(n<0)
+  {
+    printf("Please enter the non-negative number");
+  }
+  else if(n==0)
+  {
+    printf("The factorial of the number %d is not defined", n);
+  }
+  else
+  {
+    printf("The factorial of the number %d is: %d", n, fact(n));
+  }
+  return 0;
+}
---
+++ fibonacciLab3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+long int table[1000];
+long int Fibo(int n)
+{
+  if(n==1 || n==2) //Base case
+   return 1;
+  else
+  {
+    if(table[n] == -1)
+    {
+      table[n] = Fibo(n-1) + Fibo(n-2);
+    }
+    return table[n];
+  } 
+}
+int main()
+{
+  long int n;
+  int i;
+  for(i=1; i<1000; i++)
+  {
+    table[i] = -1;
+  }
+  printf("Enter a number: ");
+  scanf("%d", &n);
+  printf("Answer is: %ld ", Fibo(n));
+  return 0;
+}
