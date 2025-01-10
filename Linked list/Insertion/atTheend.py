60c2e958f84cc9b559b14b33155143be27bd65d6
Pratkshya <pratikshyaparajuli37@gmail.com>
2025-01-10 13:05:40 +0545

    Linked list

---
+++ Linked list/Insertion/atTheend.py
@@ -0,0 +1,27 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+nodeA = Node(12)
+nodeB = Node(3)
+nodeC = Node(5)
+nodeD = Node(9)
+
+nodeA.next = nodeB
+nodeB.next = nodeC
+nodeC.next = nodeD
+
+newNode = Node(77)
+head = nodeA
+current = head
+while current.next is not None:
+    current = current.next
+current.next = newNode
+
+current = head
+while current is not None:
+    print(current.data, end=" -->  ")
+    current = current.next
+print("None")      
+        
