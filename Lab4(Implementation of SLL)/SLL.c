0f7454fd4259ca24f6a13022eb0b9f5da8f8e203
Pratkshya <pratikshyaparajuli37@gmail.com>
2025-01-15 12:16:14 +0545

    lab4

---
+++ Lab4(Implementation of SLL)/SLL.c
@@ -0,0 +1,137 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct SLL
+{
+    int data;
+    struct  SLL *next;
+
+};
+struct SLL *first, *last=NULL;
+struct SLL* create_node(int element)
+{
+    struct SLL *newNode;
+    newNode = (struct SLL*) malloc(sizeof(struct SLL));
+    if (newNode != NULL)
+    {
+        newNode -> data = element;
+        newNode -> next = NULL;
+    }
+    return newNode;
+};
+void insert_at_the_beginning(int element)
+{
+    struct SLL *newNode = create_node(element);
+    if (newNode == NULL)
+    {
+        printf("Memort Allocation Failed\n");
+    }
+    else
+    {
+        if(first == NULL)
+            first = last = newNode;
+        else
+        {
+            newNode -> next = first;
+            first = newNode;
+        }
+    }
+}
+void insert_at_the_end(int element)
+{
+    struct SLL *newNode = create_node(element);
+    if (newNode == NULL)
+    {
+        printf("Memort Allocation Failed\n");
+    }
+    else
+    {
+        if(first == NULL)
+            first = last = newNode;
+        else
+        {
+            last -> next = newNode;
+            last = newNode;
+        }
+    }
+}
+void insert_at_the_specific_position(int element, int position)
+{
+    struct SLL *newNode;
+    struct SLL *temp;
+    newNode = (struct SLL*) malloc(sizeof(struct SLL));
+    newNode -> data = element;
+    newNode -> next = NULL;
+    if(position == 1)    
+    {
+        newNode -> next = first;
+        first = newNode;
+    }
+    else
+        temp = first;
+        for(int i = 1; i < position - 1; i++)
+        {
+            temp = temp -> next;
+        }
+        newNode -> next = temp -> next;
+        temp -> next = newNode;
+        printf("%d is inserted\n", newNode -> data);
+}
+
+void delete_at_the_beginning()
+{
+    struct SLL *temp = first;
+    if(first == NULL)
+        printf("List is empty\n");
+    else if (first -> next == NULL)
+    {
+        first = last = NULL;
+        printf("%d is going to be deleted\n", temp -> data);
+        free(temp);
+    }  
+    else
+    {
+        temp = first;
+        first = first -> next;
+        printf("%d is deleted\n", temp -> data);
+        free(temp);
+    }  
+}
+void deletion_at_the_end(element)
+{
+    
+}
+void display()
+{
+    struct SLL *temp = first;
+    if(first == NULL)
+    {
+        printf("Empty list\n");
+    }
+    else
+    {
+        do
+        {
+            printf("%d -> ", temp -> data);
+            temp = temp -> next;
+        } while (temp != NULL);
+        printf("NULL\n");
+        
+    }
+}
+
+int main()
+{
+    insert_at_the_beginning(100);
+    display();
+    insert_at_the_beginning(200);
+    display();
+    insert_at_the_beginning(500);
+    display();
+    insert_at_the_specific_position(600, 3);
+    display();
+    delete_at_the_beginning();
+    display();
+    return 0;
+}
+
